5f51cd24a94c08496d5709aa75c212ce936d0b1f
Rocky Xu <wesharer@gmail.com>
2013-12-04 17:08:01 +0800

    Add VehicleDB unit test and fixed bug

---
+++ DNT.Diag.Test/VehicleDBTest.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.IO;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using DNT.Diag.DB;
+
+
+namespace DNT.Diag.Test
+{
+  [TestClass]
+  public class VehicleDBTest
+  {
+    private VehicleDB db;
+
+    [TestInitialize]
+    public void Initialize()
+    {
+      db = null;
+    }
+
+    [TestMethod]
+    public void Test()
+    {
+      try
+      {
+        db = new VehicleDB(Path.GetFullPath("."), "DCJ");
+        db.Open();
+      }
+      catch (FileNotFoundException ex)
+      {
+        Assert.Fail(ex.Message);
+      }
+      finally
+      {
+        try
+        {
+          db.Close();
+        }
+        finally
+        {
+        }
+      }
+
+      try
+      {
+        db = new VehicleDB("", "");
+        db.Open();
+        Assert.Fail("Vehicle DB should open fail but success!");
+      }
+      catch (FileNotFoundException ex)
+      {
+        // this is what we want to
+      }
+      finally
+      {
+        try
+        {
+          db.Close();
+        }
+        finally
+        {
+        }
+      }
+    }
+  }
+}
